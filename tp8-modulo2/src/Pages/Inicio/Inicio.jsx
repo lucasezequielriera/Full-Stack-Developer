8a043041464e19acce8b434f2b26027a45e6e828
Lucas Riera <lucasezequielriera@hotmail.com>
2021-07-22 20:53:03 -0300

    Update import

--- tp8-modulo2/src/Pages/Inicio/Inicio.jsx
+++ tp8-modulo2/src/Pages/Inicio/Inicio.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import './Inicio.css';
 import firebase from '../../config/firebase';
-import { getProductos } from '../../services/ItemServices';
+// import { getProductos } from '../../services/ItemServices';
 
 export default function Inicio() {
 
